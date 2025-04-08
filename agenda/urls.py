ee3a6a85033ff9177970e422162cd21870f62f1f
Kamila Vit�ria de Souza Cardozo <56631494818@>
2025-04-08 11:12:25 -0300

    teste 1

--- agenda/urls.py
+++ agenda/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('api/servicos/', views.criar_ver, name='criar_ver'),
+    path('api/servicos/<int:pk>', views.detalhes_servico, name='detalhes_servico'),
+
+    path('api/servicos/', views.creat_read, name='creat_read'),
+    path('api/servicos/<int:pk>', views.detalhes_agentamento, name='detalhes_agentamento'),
+
+
+
     path('servicos/', views.servicos, name='servicos'),
     path('agendamentos/', views.agendamentos, name='agendamentos'),
     path('criar/servico/', views.criar_servico, name='criar_servico'),
@@ -9,3 +17,5 @@ urlpatterns = [
     path('criar/agendamento/', views.criar_agendamento, name='criar_agendamento'),
     path('detalhes/agendamento/', views.detalhes_agentamento, name='detalhes_agentamento'),
 ]
+
+'consultas/<int:pk>/'
--- agenda/views.py
+++ agenda/views.py
@@ -7,16 +7,13 @@ from rest_framework import status #importei o status que mostra os status das co
 # Create your views here.
 
 # de serviço
-# ver todos os servicos
-@api_view(['GET'])
-def servicos(request):
-    servico = Servico.objects.all()
-    serializer =  PrimeiroSerializer(servico, many=True)
-    return redirect ("servicos")
 
-# criar servico
-@api_view(['POST'])
-def criar_servico(request):
+@api_view(['GET','POST'])
+def criar_ver(request):
+    if request.method == 'GET':
+            servico = Servico.objects.all()
+            serializer =  PrimeiroSerializer(servico, many=True)
+            return Response(serializer.data)
     if request.method == 'POST':
         serializer = PrimeiroSerializer(data=request.data, many=isinstance(request.data, list))
         if serializer.is_valid():
@@ -24,6 +21,7 @@ def criar_servico(request):
             return Response (serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+
 # pesquisar consultas por id
 @api_view(['GET'])
 def detalhes_servico(request,pk):
@@ -34,26 +32,52 @@ def detalhes_servico(request,pk):
     serializer = PrimeiroSerializer(servicos)
     return Response(serializer.data)
 
+# # ver todos os servicos
+# @api_view(['GET'])
+# def servicos(request):
+#     servico = Servico.objects.all()
+#     serializer =  PrimeiroSerializer(servico, many=True)
+#     return redirect ("servicos")
+
+# # criar servico
+# @api_view(['POST'])
+# def criar_servico(request):
+#     if request.method == 'POST':
+#         serializer = PrimeiroSerializer(data=request.data, many=isinstance(request.data, list))
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response (serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# pesquisar consultas por id
+# @api_view(['GET'])
+# def detalhes_servico(request,pk):
+#     try:
+#         servicos = Servico.objects.get(pk=pk)
+#     except Servico.DoesNotExist:
+#         return Response({'erro': 'Esse serviço é inexistente'}, status=status.HTTP_404_NOT_FOUND)
+#     serializer = PrimeiroSerializer(servicos)
+#     return Response(serializer.data)
+
 
 
 # de agendamento
-# ver todos os agendamentos
-@api_view(['GET'])
-def agendamentos(request):
-    agendamentos = Agendamento.objects.all()
-    serializer =  SegundoSerializer(agendamentos, many=True)
-    return redirect ("agendamentos")
 
-# criar agendamento
-@api_view(['POST'])
-def criar_agendamento(request):
+
+
+@api_view(['GET','POST'])
+def creat_read(request):
+    if request.method == 'GET':
+            Agendamentos = Agendamento.objects.all()
+            serializer =  SegundoSerializer(Agendamentos, many=True)
+            return Response(serializer.data)
     if request.method == 'POST':
         serializer = SegundoSerializer(data=request.data, many=isinstance(request.data, list))
         if serializer.is_valid():
             serializer.save()
             return Response (serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
 # pesquisar agendamento por id
 @api_view(['GET'])
 def detalhes_agentamento(request,pk):
@@ -63,3 +87,38 @@ def detalhes_agentamento(request,pk):
         return Response({'erro': 'Esse serviço é inexistente'}, status=status.HTTP_404_NOT_FOUND)
     serializer = SegundoSerializer(agendamentos)
     return Response(serializer.data)
+
+
+
+
+
+
+
+
+
+# # ver todos os agendamentos
+# @api_view(['GET'])
+# def agendamentos(request):
+#     agendamentos = Agendamento.objects.all()
+#     serializer =  SegundoSerializer(agendamentos, many=True)
+#     return redirect ("agendamentos")
+
+# # criar agendamento
+# @api_view(['POST'])
+# def criar_agendamento(request):
+#     if request.method == 'POST':
+#         serializer = SegundoSerializer(data=request.data, many=isinstance(request.data, list))
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response (serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# # pesquisar agendamento por id
+# @api_view(['GET'])
+# def detalhes_agentamento(request,pk):
+#     try:
+#         agendamentos = Agendamento.objects.get(pk=pk)
+#     except Agendamento.DoesNotExist:
+#         return Response({'erro': 'Esse serviço é inexistente'}, status=status.HTTP_404_NOT_FOUND)
+#     serializer = SegundoSerializer(agendamentos)
+#     return Response(serializer.data)
