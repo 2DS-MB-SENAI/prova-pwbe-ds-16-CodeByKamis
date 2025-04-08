11795a8d0d51d023f659444ed3b1bff4e17f922a
Kamila Vit�ria de Souza Cardozo <56631494818@>
2025-04-08 10:11:12 -0300

    teste 1

--- clinica/views.py
+++ clinica/views.py
@@ -32,7 +32,7 @@ def criar_consulta(request):
         serializer = SegundoSerializer(data=request.data, many=isinstance(request.data, list))
         if serializer.is_valid():
             serializer.save()
-            return Response (serializer.data, status=status.HTTP_201_CREATED)
+            return Response (serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 # pesquisar consultas por id
